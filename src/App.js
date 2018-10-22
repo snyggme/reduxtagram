85d0ea1498b6b19663b70afa2da73f602983afea
Бабахин Роман <babahin@ad.gksnab.ru>
2018-10-22 17:48:08 +0300

    initial \xF0\x9F\x99\x8C

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		};
+	}
+	render() {
+		return (
+			<div>
+				<h1>
+					<Link to='/'>Reduxtagram</Link>
+				</h1>
+			</div>
+		)
+	}
 }
 
 export default App;
