d82ac17dc0ac1382a53d965e65c0b5f402215c5b
snyggme <rcbabahin@gmail.com>
2018-10-30 21:37:57 +0300

    Updates

---
+++ precache-manifest.763f7d97994bb9c446de07bbe6f691cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "532b256e564cfa3242d2",
+    "url": "/reduxtagram/static/js/runtime~main.532b256e.js"
+  },
+  {
+    "revision": "76e3b5e22196f557ba90",
+    "url": "/reduxtagram/static/js/main.76e3b5e2.chunk.js"
+  },
+  {
+    "revision": "7af48f9eb7e518429247",
+    "url": "/reduxtagram/static/js/1.7af48f9e.chunk.js"
+  },
+  {
+    "revision": "76e3b5e22196f557ba90",
+    "url": "/reduxtagram/static/css/main.199dbabd.chunk.css"
+  },
+  {
+    "revision": "e9f45d07760519225caa27385d2638c4",
+    "url": "/reduxtagram/index.html"
+  }
+];
